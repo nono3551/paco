c0f8805eb5e096c5428677fff8e8cec3ad32ee93
Michal Záhradník <nono3551@gmail.com>
2020-12-26 21:04:51 +0100

    Refactor

--- Paco/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ Paco/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -24,8 +24,7 @@ namespace Paco.Areas.Identity
 
         protected override TimeSpan RevalidationInterval => TimeSpan.FromMinutes(30);
 
-        protected override async Task<bool> ValidateAuthenticationStateAsync(
-            AuthenticationState authenticationState, CancellationToken cancellationToken)
+        protected override async Task<bool> ValidateAuthenticationStateAsync(AuthenticationState authenticationState, CancellationToken cancellationToken)
         {
             // Get the user manager from a new scope to ensure it fetches fresh data
             var scope = _scopeFactory.CreateScope();
