ec6c56c824acacdf5279252d37a657d71598500c
Michal Záhradník <nono3551@gmail.com>
2021-02-28 23:13:37 +0100

    Displaying permissions

---
+++ Paco/Data/DatabaseRepository/RoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Paco.Data.Entities.Identity;
+
+namespace Paco.Data.DatabaseRepository
+{
+    public static class RoleRepository
+    {
+        public static Role GetRoleWithUsers(this DbSet<Role> roles, Guid roleId)
+        {
+            return roles.Where(r => r.Id == roleId).Include(s => s.Users).FirstOrDefault();
+        }
+    }
+}
---
+++ Paco/Data/DatabaseRepository/RoleSystemPermissionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Paco.Data.Entities;
+
+namespace Paco.Data.DatabaseRepository
+{
+    public static class RoleSystemPermissionRepository
+    {
+        public static List<RoleSystemPermission> GetPermissionsWithSystemsForRole(this DbSet<RoleSystemPermission> roleSystemPermissions, Guid roleId)
+        {
+            return roleSystemPermissions.Where(r => r.Role.Id == roleId)
+                .Include(s => s.ManagedSystem)
+                .ToList();
+        }
+    }
+}
