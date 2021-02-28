c86ec98ffac488401600f3df03f69c9618121e4b
Michal Záhradník <nono3551@gmail.com>
2021-02-28 18:29:53 +0100

    Role validation

---
+++ Paco/Validations/CustomValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Paco.Validations
+{
+    public class CustomValidator : ComponentBase
+    {
+        private ValidationMessageStore _messageStore;
+
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CustomValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. " +
+                    $"For example, you can use {nameof(CustomValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            _messageStore = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (s, e) => _messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) => _messageStore.Clear(e.FieldIdentifier);
+        }
+
+        public void DisplayErrors(Dictionary<string, IEnumerable<string>> errors)
+        {
+            foreach (var error in errors)
+            {
+                _messageStore.Add(CurrentEditContext.Field(error.Key), error.Value);
+            }
+
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        public void ClearErrors()
+        {
+            _messageStore.Clear();
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+}
