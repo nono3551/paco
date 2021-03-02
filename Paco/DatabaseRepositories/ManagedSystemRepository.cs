df20b41b179d913d81cd740fa842457b22a33969
Michal Záhradník <nono3551@gmail.com>
2021-03-02 11:54:46 +0100

    Refactor

--- Paco/DatabaseRepositories/ManagedSystemRepository.cs
+++ Paco/DatabaseRepositories/ManagedSystemRepository.cs
@@ -8,13 +8,13 @@ namespace Paco.DatabaseRepositories
 {
     public static class ManagedSystemRepository
     {
-        public static List<ManagedSystem> GetSystemsForUser(this DbSet<ManagedSystem> systems, User user)
+        public static List<ManagedSystem> GetManagedSystemsForUser(this DbSet<ManagedSystem> systems, User user)
         {
             return systems.Where(s => s.RoleManagedSystemPermissions.Any(p => p.Permissions > Permissions.None && p.Role.Users.Contains(user)))
                 .Include(s => s.RoleManagedSystemPermissions.Where(rp => rp.Role.Users.Contains(user))).ToList();
         }
         
-        public static List<ManagedSystem> GetSystemsForTermWithRolePermissionsForRole(this DbSet<ManagedSystem> systems, Role role, string term, int limit = 15)
+        public static List<ManagedSystem> GetManagedSystemsForTermWithRolePermissionsForRole(this DbSet<ManagedSystem> systems, Role role, string term, int limit = 15)
         {
             var asd = systems.Where(x => x.Name.Contains(term) || x.Hostname.Contains(term))
                 .Include(x => x.RoleManagedSystemPermissions.Where(y => y.Role.Id == role.Id))
@@ -25,7 +25,7 @@ namespace Paco.DatabaseRepositories
             return asd;
         }
         
-        public static List<ManagedSystem> GetSystemsWithRolePermissionsForRole(this DbSet<ManagedSystem> systems, Role role)
+        public static List<ManagedSystem> GetManagedSystemsWithRolePermissionsForRole(this DbSet<ManagedSystem> systems, Role role)
         {
             return systems.Include(x => x.RoleManagedSystemPermissions.Where(y => y.Role == role))
                 .Where(x => x.RoleManagedSystemPermissions.Any(y => y.Role == role))
